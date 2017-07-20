cc92122887bbcafee6fab29ff74bf9b7d1ea0e28
Danushka Herath <Danushkaherath96@gmail.com>
2017-07-20 14:30:44 +0530

    Add files via upload

--- test.php
+++ test.php
@@ -21,19 +21,13 @@ require_once('process/permissions.php');
 
 <p align="center"><img src="img/logo.png"></p>
 <h1 align="center"> Wood Arts Comapany Managment System</h1>
-<?php
-require_once('process/menu.php');
-echo $menu;
- ?>
+
 <h2>You have logged in as,
 	<?php echo $_SESSION['login_user'];?>
 </h2>
-<div class="pbox">
-<p><b><u>Permissions</p></u></b>
-<?php echo $permissionstat; ?>
-</div>
+
 <br>
-<img src="img/index.jpg" width="800" height="400">
+
 
 </body>
 </html>
